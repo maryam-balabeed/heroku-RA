09bce78492e27448dc70c57d94fad5a9a174d709
maryam-balabeed <maryam.balabeed@tuwaiq.edu.sa>
2022-01-23 15:03:34 +0300

    second commit

--- frontend/src/Components/Favorit.js
+++ frontend/src/Components/Favorit.js
@@ -7,7 +7,7 @@ export default function Favorit( {token} ) {
     const [Favorit, setFavorit] = useState([]);
      useEffect(async () => {          
          if(token) {
-             const Fav = await axios.get("http://localhost:5000/Favorit", {
+             const Fav = await axios.get("https://heroku-dep-raghad.herokuapp.com/Favorit", {
                  headers: {authorization: "Bearer " + token},
              });
              setFavorit(Fav.data);
